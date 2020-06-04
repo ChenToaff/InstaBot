9eae28320aaf819cc03926958d1cb430a7336489
unknown <toaff100@gmail.com>
2020-06-04 15:42:22 +0300

    added readme

---
+++ dataBase.py
@@ -0,0 +1,32 @@
+from sqlite3 import Error,connect
+
+class Db:
+    def __init__(self, path):
+        self.connection = None
+        try:
+            self.connection = connect(path)
+            print("Connection to SQLite DB successful")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+        self.cursor = self.connection.cursor()
+
+    def query(self, query):
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    def read_query(self, query):
+        try:
+            self.cursor.execute(query)
+            result = self.cursor.fetchall()
+            return result
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    def close(self):
+        self.connection.close()
+
+
